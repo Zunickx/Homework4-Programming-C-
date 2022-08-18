81073412a90f681550b5892dd30b559b728efb5b
Zunickx <Zunickx@gmail.com>
2022-08-18 23:03:07 +0300

    Task001 Задача на возведение в степень, при помощи цикла

---
+++ Task001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.WriteLine("Введите число a:");
+string a = Console.ReadLine()!;
+Console.WriteLine("Введите число b:");
+string b = Console.ReadLine()!;
+
+int numA = int.Parse(a);
+int numB = int.Parse(b);
+
+if(numB == 0)
+{
+    Console.WriteLine("1");
+}
+else if(numB == 1)
+{
+    Console.WriteLine(numA);
+}
+else
+{
+    int result = numA;
+    for(int i = 1; i < numB; i++)
+    {
+        result = result * numA;
+    }
+
+    Console.WriteLine(result);
+}
